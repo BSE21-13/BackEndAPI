7e519815ae0501939ab19ac053a9fd6b3be37ec7
michael-aries11 <kabilamitchel@gmail.com>
2021-12-13 15:35:41 +0300

    Added requirements.txt file for dependency tracking

--- app.py
+++ app.py
@@ -4,25 +4,58 @@ from dotenv import load_dotenv
 from flask_pymongo import PyMongo
 from bson.json_util import dumps
 from bson.objectid import ObjectId
-
 from flask import jsonify, request
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_cors import CORS, cross_origin
+
 
 load_dotenv() 
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 app.secret_key = "secret_key"
 app.config["MONGO_URI"] = os.getenv("MONGO_URI")
 mongo  = PyMongo(app)
 
 @app.route('/search', methods=['GET'])
+@cross_origin()
 def query_resource():
-   resp = jsonify('Resource Queried, Results sent succesffuly.')
-   resp.status_code = 200
-   return resp
+    queryString = request.args.get('q')
+    results = {
+        "keywords":[queryString, "law", "state", "prosecution", "police", "unfair", "appeal"], 
+        
+    "results" : [{
+        
+        "chapter":"Chapter 1",
+        "text":" The  people  shall  express  their  will  and  consent  on  who  shall govern them and how\nthey should be governed, through regular, free and fair elections of their representatives or through referenda.",
+    },
+    {
+        "chapter":"Chapter 13",
+        "text":" The  people  shall  express  their  will  and  consent  on  who  shall govern them and how\nthey should be governed, through regular, free and fair elections of their representatives or through referenda.",
+    },
+    {
+        "chapter":"Chapter 11",
+        "text":" The  people  shall  express  their  will  and  consent  on  who  shall govern them and how\nthey should be governed, through regular, free and fair elections of their representatives or through referenda.",
+    },
+    {
+        "chapter":"Chapter 8",
+        "text":" The  people  shall  express  their  will  and  consent  on  who  shall govern them and how\nthey should be governed, through regular, free and fair elections of their representatives or through referenda.",
+    },
+    {
+        "chapter":"Chapter 5",
+        "text":" The  people  shall  express  their  will  and  consent  on  who  shall govern them and how\nthey should be governed, through regular, free and fair elections of their representatives or through referenda.",
+    }
+    ]}
+
+    resp = jsonify(results)
+    resp.status_code = 200
+    return resp
 
 
 @app.errorhandler(404)
+@cross_origin()
 def not_found(error=None):
     message = {
         'status': 404,
